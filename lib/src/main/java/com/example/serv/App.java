1012555922c6949dda0bc82bd460f3ffe6ccf354
Mark <xhang0o0@gmail.com>
2016-05-04 01:32:03 -0700

    1

--- lib/src/main/java/com/example/serv/App.java
+++ lib/src/main/java/com/example/serv/App.java
@@ -10,7 +10,7 @@ public class App {
 
         System.out.println("Sending POST to GCM");
 
-        String apiKey = "AIzaSyC_oi5v-QwyHirz_xbDzwcdcAROWrZ-xwc";
+        String apiKey = "AIzaSyDDmQgAayqPVa0g954ieLN7GVrcB5Uy4SQ";
         Content content = createContent();
 
         Post2Gcm.post(apiKey, content);
@@ -19,7 +19,7 @@ public class App {
     public static Content createContent() {
         Content c = new Content();
 
-        c.addRegId("APA91bGB-ZbeyCtLP_QRNQE5zu4Y-1LgRlTFiIMh2vDrhtgVwL5vTo5DYiE_YFo4ftOjxk4Nihykt_LSTPL9T_7FX64I7EQoe-l_eQ_4-T36BwAdOgAioktNUE1fwFoaPe6fdzQ1Aayx");
+        c.addRegId("APA91bE7gJmMUuqnxSzXvFU-5W1vySemX2gOadFxZXVxHCpMnHnh7rGfrk37G1aKWSa9oMIfRtAqb5TW1de1VyUwI8-uY6qq635vlerdJFFNucUBDbZ8chR3jTnZoknY9pXmF-KTxLXF");
 
         c.createData("Working!!", "Test message");
 
